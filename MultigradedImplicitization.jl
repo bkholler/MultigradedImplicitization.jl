8813b09c2753d3c320cb06fca04c90f2d731feb0
antonydellavecchia <antonydellavecchia@gmail.com>
2023-12-05 15:33:24 +0100

    some updates

--- MultigradedImplicitization.jl
+++ MultigradedImplicitization.jl
@@ -1,11 +1,16 @@
-using Oscar
+using Revise, Oscar
 using Distributed
 
+n_cores = 5
+addprocs(n_cores)
+
+@everywhere using Oscar
+# changing the type Any to be more specifice will result in speedup
+(_, _, params_channels) = set_channels(Any, Any, Any)
 
 # Some generic helper functions which I could not find in Julia or OSCAR
 # Determine the pivots by computing the RREF and then looking for the first nonzero entry of each row
 function pivots(M)
-
     (r, R) = rref(M)
     pivots = []
 
@@ -22,21 +27,13 @@ function pivots(M)
     return pivots
 end
 
-
-
-
 # given a vector of monomials mons and a row vector M of polynomials finds a matrix coeffs such that mons*coeffs = M
 # this replaces the function coefficients which we use in macaulay2
-function mons_and_coeffs(mons, M)
-
+@everywhere function mons_and_coeffs(mons, M)
     coeffs = [[coeff(f, mons[j]) for f in M] for j in 1:length(mons)]
 
     return matrix(QQ, coeffs)
 end
-
-
-
-
 
 # compute the homogeneity space of an ideal and return it in row reduced echelon form
 function homogeneity_space(I)
@@ -58,7 +55,6 @@ end
 # finds the maximal grading of the kernel of a polynomial map phi
 # by computing the homogeneity space of the elimination ideal
 function max_grading(phi)
-
     codom = codomain(phi)
     dom = domain(phi)
     elim_ring, z = polynomial_ring(QQ, vcat([string(i) for i in symbols(codom)], [string(i) for i in symbols(dom)]))
@@ -69,19 +65,12 @@ function max_grading(phi)
     return homogeneity_space(ideal([lift_dom(x) - lift_codom(phi(x)) for x in gens(dom)]))
 end
 
-
-
-
-
 # computes the basis for domain in degree deg but removes all monomials 
 # which correspond to previously computed relations G
-function find_basis_in_degree(domain, deg, prev_gens) 
-
+@everywhere function find_basis_in_degree(domain, deg, prev_gens) 
     if length(prev_gens) == 0
-        
         return monomial_basis(domain, deg)
     end
-
     gen_shifts = reduce(vcat, [[g*b for b in monomial_basis(domain, deg - degree(g))] for g in prev_gens])
     mons = unique!(reduce(vcat, [collect(monomials(f)) for f in gen_shifts]))
     coeffs = mons_and_coeffs(mons, gen_shifts)
@@ -90,38 +79,34 @@ function find_basis_in_degree(domain, deg, prev_gens)
     return [m for m in monomial_basis(domain, deg) if !(m in bad_monomials)]
 end
 
-
-
-
-
-function component_of_kernel(deg, phi, prev_gens)
+@everywhere function component_of_kernel(deg, phi, prev_gens)
 
     mon_basis = find_basis_in_degree(domain(phi), deg, prev_gens)
     image_polys = [phi(m) for m in mon_basis]
     mons = unique!(reduce(vcat, [collect(monomials(f)) for f in image_polys]))
     coeffs = mons_and_coeffs(mons, image_polys)
     (r, K) = nullspace(coeffs)
-
+    println(K, typeof(K))
     return mon_basis*K
 end
 
-
-
-
-
-
-
 function components_of_kernel(d, phi)
+    put_params(params_channels, phi)
+    
     A = max_grading(phi)[:, (ngens(codomain(phi))+1):end]
     #A = vcat(matrix(ZZ, [[1 for i in 1:ncols(A)]]), A)
     graded_dom = grade(domain(phi), A)[1]
     grA = grading_group(graded_dom)
+    
+    put_params(params_channels, grA)
+    put_params(params_channels, graded_dom)
+        
     total_deg_dom = grade(domain(phi), [1 for i in 1:ngens(domain(phi))])[1]
-    phi = hom(graded_dom, codomain(phi), [phi(x) for x in gens(domain(phi))]) 
+    phi = hom(graded_dom, codomain(phi), [phi(x) for x in gens(domain(phi))])
+
     gens_dict = Dict{GrpAbFinGenElem, Vector{MPolyDecRingElem}}()
 
     for i in 1:d
-        
         all_mons = [graded_dom(m) for m in monomial_basis(total_deg_dom, [i])]
         all_degs = unique!([degree(m) for m in all_mons])
 
@@ -130,32 +115,43 @@ function components_of_kernel(d, phi)
         else
             prev_gens = reduce(vcat, values(gens_dict))
         end
+        println(i)
+        merge!(gens_dict, Dict(zip(
+            all_degs,
+            pmap(component_of_kernel,
+                 all_degs,
+                 [phi for _ in all_degs] ,
+                 [prev_gens for _ in all_degs]))))
 
-        # this can be parallelized
-        for deg in all_degs
-            values = component_of_kernel(deg, phi, prev_gens)
-            gens_dict[deg] = values
-        end
     end
 
     return gens_dict
 end
 
 
-
-
 # Example: A product of segree products
 R, (x11, x12, x21, x22, y11, y12, y21, y22) = QQ["x11", "x12", "x21", "x22", "y11", "y12", "y21", "y22"]
+put_params(params_channels, R)
+
 S, (s1, s2, t1, t2, m1, m2, n1, n2) = QQ["s1", "s2", "t1", "t2", "m1", "m2", "n1", "n2"]
+put_params(params_channels, S)
+
 phi = hom(R, S, [s1*t1, s1*t2, s2*t1, s2*t2, m1*n1, m1*n2, m2*n1, m2*n2])
+put_params(params_channels, phi)
+
 A = max_grading(phi)[:, 9:end]
+
 B = vcat(matrix(QQ, [[1 for i in 1:ncols(A)]]), A)
 R, (x11, x12, x21, x22, y11, y12, y21, y22) = grade(R, A)
+grA = grading_group(R)
+
+put_params(params_channels, grA)
+put_params(params_channels, R)
+
 phi = hom(R, S, [s1*t1, s1*t2, s2*t1, s2*t2, m1*n1, m1*n2, m2*n1, m2*n2])
 f1 = x11*x22 - x12*x21
 f2 = y11*y22 - y12*y21
 G = [f1,  f2];
-grA = grading_group(R)
 
 deg = grA([1,1,1,1,0,0,0,0])
 
@@ -163,4 +159,4 @@ dicts = components_of_kernel(2, phi)
 #dict = dicts[2]
 #reduce(vcat, values(dict))
 
-
+map(rmprocs, workers())
